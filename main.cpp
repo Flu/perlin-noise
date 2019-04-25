66de5742b7547490ba302336041249e829aa304b
Fluturel Adrian <fluturel.adrian@gmail.com>
2019-04-25 05:07:01 +0300

    Added validation layers for debugging purposes

--- main.cpp
+++ main.cpp
@@ -11,6 +11,19 @@
 constexpr int WIDTH = 800;
 constexpr int HEIGHT = 600;
 
+#define NDEBUG 0
+
+const std::vector<const char*> validationLayers =
+  {
+   "VK_LAYER_LUNARG_standard_validation"
+  };
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApp {
 public:
   void run();
@@ -18,7 +31,37 @@ private:
   GLFWwindow *window;
   VkInstance instance;
 
+  bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (auto layerName : validationLayers) {
+      bool layerFound = false;
+      
+      for (const auto& layerProperties : availableLayers) {
+        if (strcmp(layerName, layerProperties.layerName) == 0) {
+	  layerFound = true;
+	  break;
+        }
+      }
+      
+      if (!layerFound) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
+  
   void createInstance() {
+
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+      throw std::runtime_error("Validation layers requested, but not available");
+    }
+    
     VkApplicationInfo appInfo = {};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello, Triangle";
